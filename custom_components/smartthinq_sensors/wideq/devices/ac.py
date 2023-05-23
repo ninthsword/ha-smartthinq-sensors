4328976e0839edb5856f93e80296bc6d928657d6
ninthsword <86150318+ninthsword@users.noreply.github.com>
2023-05-24 00:18:13 +0900

    Update ac.py

--- custom_components/smartthinq_sensors/wideq/devices/ac.py
+++ custom_components/smartthinq_sensors/wideq/devices/ac.py
@@ -237,8 +237,8 @@ class ACHSwingMode(Enum):  ####
     """The swing mode for an AC/HVAC device."""
 
     정지 = "@OFF"         ####
-    좌측 = "@LEFT_ON"     ####
-    우측 = "@RIGHT_ON"    ####
+    좌측회전 = "@LEFT_ON"     ####
+    우측회전 = "@RIGHT_ON"    ####
     회전 = "@ALL_ON"      ####
  
 class JetMode(Enum):
