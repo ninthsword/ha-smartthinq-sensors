8b50dc3d959e65dfb04033fca468bbf4c18032d4
ninthsword <86150318+ninthsword@users.noreply.github.com>
2025-04-21 10:27:45 +0900

    Update const.py

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -1,6 +1,6 @@
 """Constants for LGE ThinQ custom component."""
 
-__version__ = "0.41.1_0.1"
+__version__ = "0.41.1_0.2"
 PROJECT_URL = "https://github.com/ninthsword/ha-smartthinq-sensors/"
 ISSUE_URL = f"{PROJECT_URL}issues"
 
